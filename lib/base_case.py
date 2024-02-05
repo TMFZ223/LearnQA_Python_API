3ae01a734be635f3c55a94d49b517ba5acf2c485
TMFZ <temur01@yandex.ru>
2024-02-05 22:58:15 +0300

    test_get_user

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,17 +1,18 @@
+
 class BaseCase:
     def get_cookie (self, response: Response, cookie_name):
         assert cookie_name in response.cookie, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
     def get_header (self, response: Response, headers_name):
-        assert headers_name in response.headers, f"Cannot find header with the name {headers_name} in the last response"
+        assert headers_name in response.headers, f"Cannot find header with the name {headers_name} in the last resoinse"
         return response.headers[headers_name]
     
-    def get_json_value(self, response: Response, name):
+    def get_json_value(self, response: Respnse, name):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecoderError:
-            assert False, f"Response is not in JSON Format. Resonse text is '{response.text}'"
+            assert False, f"Response is not in JSON Format. Resonse text is '{resonse.text}'"
 
         assert name in response_as_dict, f"Response Json doesn't have ant key '{name}'"
 
