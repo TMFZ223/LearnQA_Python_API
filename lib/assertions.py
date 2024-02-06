8c853217f327bd61553b65b435df586e397f7f5f
TMFZ <temur01@yandex.ru>
2024-02-06 21:41:58 +0300

    test method delete

--- lib/assertions.py
+++ lib/assertions.py
@@ -3,44 +3,11 @@ import json
 
 class Assertions:
     @staticmethod
-    def assert_json_value_by_name(resoponse: Response, name, expected_value, error_message):
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
         try:
-            response_as_dict = resoponse.json()
+            response_as_dict = response.json()
         except json.JSONDecodeError:
-            assert False, f"Response id not in json format. response text is '{response.text}'"
+            assert False, f"Response is not in json format. response text is '{response.text}'"
 
-        assert name is response_as_dict, f"Response json doesn't have key '{name}'"
+        assert name in response_as_dict, f"Response json doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
-
-    @staticmethod
-    def assert_json_has_key(response: Response, name):
-        try:
-            response_as_dict = response.json()
-        except json.JSONDecodeError:
-            assert False, f"Response id not in json format. response text is '{response.text}'"
-
-        assert name in response_as_dict, f"Response json doesn't have key '{name}'"
-
-    @staticmethod
-    def assert_json_has_key(response: Response, name: list):
-        try:
-            response_as_dict = response.json()
-        except json.JSONDecodeError:
-            assert False, f"Response id not in json format. response text is '{response.text}'"
-    
-    for name in names:
-        assert name in response_as_dict, f"Response json doesn't have key '{name}'"
-
-    @staticmethod
-    def assert_json_has_not_key(response: Response, name):
-        try:
-            response_as_dict = response.json()
-        except json.JSONDecodeError:
-            assert False, f"Response id not in json format. response text is '{response.text}'"
-
-        assert name not in response_as_dict, f"Response json shouldn't have key '{name}'. But it's present"
-
-    @staticmethod
-    def assert_code_status(response: Response, expected_status_code):
-        assert response.status_code == expected_status_code,  \
-            f"Unexpected status code! expected: {expected_status_code}, Actual: {response.status_code}"
